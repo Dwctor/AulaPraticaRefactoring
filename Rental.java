a3fa69e4d535fade3659a2b661a27f708fda0fc0
Kael Soares Augusto <kael10@mail.com>
2022-12-05 11:55:32 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -3,25 +3,8 @@ public class Rental {
    private Movie _movie;
    private int _daysRented;
 
-   public double getCharge() {
-      double thisAmount = 0;
-      //determine amounts for a Rental
-      switch (getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (getDaysRented() > 2)
-               thisAmount += (getDaysRented() - 2) * 1.5;
-               break;
-         case Movie.NEW_RELEASE:
-            thisAmount += getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (getDaysRented() > 3)
-               thisAmount += (getDaysRented() - 3) * 1.5;
-            break;
-      }
-      return thisAmount;
+   public double getCharge(){
+      return _movie.getCharge(_daysRented);
    }
 
    public int getFrequentRenterPoints() {
